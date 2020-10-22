e72fb62300968c283ed5c233203240e2cd730032
Fahmi <65661058+fahmi1597@users.noreply.github.com>
2020-10-22 22:24:05 +0700

    Display eth0 IP if tun0 is not used, call 'ref' to refresh the prompt

--- .htbon.sh
+++ .htbon.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 htbip=$(ifconfig tun0 2>/dev/null | grep netmask | awk '{print $2}')
+showip=$(ifconfig eth0 2>/dev/null | grep netmask | awk '{print $2}')
 
 if [[ $htbip == *"10.10"* ]]
 then
    echo "$htbip"
 else
-   echo ""
+   echo "$showip"
 fi
